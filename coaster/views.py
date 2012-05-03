b7515ea1ea04f2196fa778404e434d62da2c06d9
Kiran Jonnalagadda <jace@pobox.com>
2012-05-03 19:10:03 +0530

    Check if both hostname and port match in get_next_url()

--- coaster/views.py
+++ coaster/views.py
@@ -27,8 +27,10 @@ def get_next_url(referrer=False, external=False):
     next_url = request.args.get('next', '')
     if not external:
         if next_url.startswith('http:') or next_url.startswith('https:') or next_url.startswith('//'):
-            # Do the domains match?
-            if urlparse.urlsplit(next_url).hostname != urlparse.urlsplit(request.url).hostname:
+            # Do the domains and ports match?
+            pnext = urlparse.urlsplit(next_url)
+            preq = urlparse.urlsplit(request.url)
+            if pnext.hostname != preq.hostname or pnext.port != preq.port:
                 next_url = ''
     if referrer:
         return next_url or request.referrer or __index_url()
