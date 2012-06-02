5c188f98add102daeab0d615118b185627783d9a
Kiran Jonnalagadda <jace@pobox.com>
2012-06-02 17:36:15 +0530

    New BaseScopedNameMixin for named objects within containers.

--- coaster/sqlalchemy.py
+++ coaster/sqlalchemy.py
@@ -2,7 +2,8 @@
 
 from __future__ import absolute_import
 from coaster import make_name
-from sqlalchemy import Column, Integer, DateTime, Unicode
+from sqlalchemy import Column, Integer, DateTime, Unicode, UniqueConstraint
+from sqlalchemy.ext.declarative import declared_attr
 from datetime import datetime
 
 
@@ -41,6 +42,39 @@ class BaseNameMixin(IdMixin, TimestampMixin):
                 checkused = lambda c: self.__class__.query.filter(self.__class__.id != self.id).filter_by(name=c).first()
             else:
                 checkused = lambda c: self.__class__.query.filter_by(name=c).first()
+            self.name = make_name(self.title, maxlength=250,
+                checkused=checkused)
+
+
+class BaseScopedNameMixin(IdMixin, TimestampMixin):
+    """
+    Base mixin class for named objects within containers. When using this,
+    you must provide an model-level attribute "parent" that is a synonym for
+    the parent object. You must also create a unique constraint on 'name' in
+    combination with the parent foreign key. Sample use case in Flask::
+
+        class Event(db.Model, BaseScopedNameMixin):
+            __tablename__ = 'event'
+            organizer_id = db.Column(db.Integer, db.ForeignKey('organizer.id'))
+            organizer = db.relationship(Organizer)
+            parent = db.synonym('organizer')
+            __table_args__ = (db.UniqueConstraint('name', 'organizer_id'),)
+
+    """
+    name = Column(Unicode(250), nullable=False)
+    title = Column(Unicode(250), nullable=False)
+
+    def __init__(self, *args, **kw):
+        super(BaseScopedNameMixin, self).__init__(*args, **kw)
+        self.make_name()
+
+    def make_name(self):
+        if self.title:
+            if self.id:
+                checkused = lambda c: self.__class__.query.filter(self.__class__.id != self.id).filter_by(
+                    name=c, parent=self.parent).first()
+            else:
+                checkused = lambda c: self.__class__.query.filter_by(name=c, parent=self.parent).first()
             self.name = make_name(self.title, maxlength=250,
                 checkused=checkused)
 
