0c14b15eed0625b5c77985d5f2e7dedd7eb674f4
Kiran Jonnalagadda <jace@pobox.com>
2012-06-19 13:44:30 +0530

    Converted all strings to Unicode

--- tests/test_models.py
+++ tests/test_models.py
@@ -7,6 +7,7 @@ from coaster.sqlalchemy import BaseMixin, BaseNameMixin, BaseScopedNameMixin, Ba
 from sqlalchemy import create_engine, Column, Integer, Unicode, UniqueConstraint, ForeignKey
 from sqlalchemy.orm import relationship, synonym, sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.exc import IntegrityError
 
 engine = create_engine('sqlite://')
 Session = scoped_session(sessionmaker(autocommit=False,
@@ -103,7 +104,7 @@ class TestCoasterModels(unittest.TestCase):
         now2 = datetime.utcnow()
         self.assertTrue(now1 < c.created_at)
         self.assertTrue(now2 > c.created_at)
-        c.content = "updated"
+        c.content = u"updated"
         self.session.commit()
         self.assertTrue(c.updated_at > now2)
         self.assertTrue(c.updated_at > c.created_at)
@@ -111,7 +112,7 @@ class TestCoasterModels(unittest.TestCase):
 
     def test_unnamed(self):
         c = self.make_container()
-        d = UnnamedDocument(content="hello", container=c)
+        d = UnnamedDocument(content=u"hello", container=c)
         self.session.add(d)
         self.session.commit()
         self.assertEqual(c.id, 1)
@@ -120,78 +121,78 @@ class TestCoasterModels(unittest.TestCase):
     def test_named(self):
         """Named documents have globally unique names."""
         c1 = self.make_container()
-        d1 = NamedDocument(title="Hello", content="World", container=c1)
+        d1 = NamedDocument(title=u"Hello", content=u"World", container=c1)
         self.session.add(d1)
         self.session.commit()
-        self.assertEqual(d1.name, 'hello')
+        self.assertEqual(d1.name, u'hello')
 
         c2 = self.make_container()
-        d2 = NamedDocument(title="Hello", content="Again", container=c2)
+        d2 = NamedDocument(title=u"Hello", content=u"Again", container=c2)
         self.session.add(d2)
         self.session.commit()
-        self.assertEqual(d2.name, 'hello1')
+        self.assertEqual(d2.name, u'hello1')
 
     def test_scoped_named(self):
         """Scoped named documents have names unique to their containers."""
         c1 = self.make_container()
-        d1 = ScopedNamedDocument(title="Hello", content="World", container=c1)
+        d1 = ScopedNamedDocument(title=u"Hello", content=u"World", container=c1)
         self.session.add(d1)
         self.session.commit()
-        self.assertEqual(d1.name, 'hello')
+        self.assertEqual(d1.name, u'hello')
 
-        d2 = ScopedNamedDocument(title="Hello", content="Again", container=c1)
+        d2 = ScopedNamedDocument(title=u"Hello", content=u"Again", container=c1)
         self.session.add(d2)
         self.session.commit()
-        self.assertEqual(d2.name, 'hello1')
+        self.assertEqual(d2.name, u'hello1')
 
         c2 = self.make_container()
-        d3 = ScopedNamedDocument(title="Hello", content="Once More", container=c2)
+        d3 = ScopedNamedDocument(title=u"Hello", content=u"Once More", container=c2)
         self.session.add(d3)
         self.session.commit()
-        self.assertEqual(d3.name, 'hello')
+        self.assertEqual(d3.name, u'hello')
 
     def test_id_named(self):
         """Documents with a global id in the URL"""
         c1 = self.make_container()
-        d1 = IdNamedDocument(title="Hello", content="World", container=c1)
+        d1 = IdNamedDocument(title=u"Hello", content=u"World", container=c1)
         self.session.add(d1)
         self.session.commit()
-        self.assertEqual(d1.url_name, '1-hello')
+        self.assertEqual(d1.url_name, u'1-hello')
 
-        d2 = IdNamedDocument(title="Hello", content="Again", container=c1)
+        d2 = IdNamedDocument(title=u"Hello", content=u"Again", container=c1)
         self.session.add(d2)
         self.session.commit()
-        self.assertEqual(d2.url_name, '2-hello')
+        self.assertEqual(d2.url_name, u'2-hello')
 
         c2 = self.make_container()
-        d3 = IdNamedDocument(title="Hello", content="Once More", container=c2)
+        d3 = IdNamedDocument(title=u"Hello", content=u"Once More", container=c2)
         self.session.add(d3)
         self.session.commit()
-        self.assertEqual(d3.url_name, '3-hello')
+        self.assertEqual(d3.url_name, u'3-hello')
 
     def test_scoped_id_named(self):
         """Documents with a container-specifc id in the URL"""
         c1 = self.make_container()
-        d1 = ScopedIdNamedDocument(title="Hello", content="World", container=c1)
+        d1 = ScopedIdNamedDocument(title=u"Hello", content=u"World", container=c1)
         self.session.add(d1)
         self.session.commit()
-        self.assertEqual(d1.url_name, '1-hello')
+        self.assertEqual(d1.url_name, u'1-hello')
 
-        d2 = ScopedIdNamedDocument(title="Hello again", content="New name", container=c1)
+        d2 = ScopedIdNamedDocument(title=u"Hello again", content=u"New name", container=c1)
         self.session.add(d2)
         self.session.commit()
-        self.assertEqual(d2.url_name, '2-hello-again')
+        self.assertEqual(d2.url_name, u'2-hello-again')
 
         c2 = self.make_container()
-        d3 = ScopedIdNamedDocument(title="Hello", content="Once More", container=c2)
+        d3 = ScopedIdNamedDocument(title=u"Hello", content=u"Once More", container=c2)
         self.session.add(d3)
         self.session.commit()
-        self.assertEqual(d3.url_name, '1-hello')
+        self.assertEqual(d3.url_name, u'1-hello')
 
-        d4 = ScopedIdNamedDocument(title="Hello", content="Third", container=c1)
+        d4 = ScopedIdNamedDocument(title=u"Hello", content=u"Third", container=c1)
         self.session.add(d4)
         self.session.commit()
-        self.assertEqual(d4.url_name, '3-hello')
+        self.assertEqual(d4.url_name, u'3-hello')
 
     def test_scoped_without_parent(self):
         d1 = ScopedNamedDocument(title=u"Hello", content=u"World")
@@ -205,19 +206,19 @@ class TestCoasterModels(unittest.TestCase):
     def test_delayed_name(self):
         c = self.make_container()
         d1 = NamedDocument(container=c)
-        d1.title = 'Document 1'
+        d1.title = u'Document 1'
         d1.make_name()
         self.session.add(d1)
         d2 = ScopedNamedDocument(container=c)
-        d2.title = 'Document 2'
+        d2.title = u'Document 2'
         d2.make_name()
         self.session.add(d2)
         d3 = IdNamedDocument(container=c)
-        d3.title = 'Document 3'
+        d3.title = u'Document 3'
         d3.make_name()
         self.session.add(d3)
         d4 = ScopedIdNamedDocument(container=c)
-        d4.title = 'Document 4'
+        d4.title = u'Document 4'
         d4.make_name()
         self.session.add(d4)
         self.session.commit()
