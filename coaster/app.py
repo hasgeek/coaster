1044d19819558cdc64de062920e3489230b02c9a
Kiran Jonnalagadda <jace@pobox.com>
2012-07-06 15:37:49 +0530

    PyLint recommendations and _prefix to mark variable as semi-private.

--- coaster/app.py
+++ coaster/app.py
@@ -5,7 +5,7 @@ import sys
 import coaster.logging
 
 
-additional_configs = {
+_additional_config = {
     'dev': 'development.py',
     'development': 'development.py',
     'test': 'testing.py',
@@ -14,29 +14,32 @@ additional_configs = {
     'production': 'production.py',
     }
 
+
 def configure(app, env):
     """
     Configure an app depending on the environment.
     """
-    load_config_from_file(app,'settings.py')
+    load_config_from_file(app, 'settings.py')
 
     additional = additional_settings_file(env)
     if additional:
-        load_config_from_file(app,additional)
+        load_config_from_file(app, additional)
 
     coaster.logging.configure(app)
+
 
 def additional_settings_file(env):
     """
     Checks to see if the environment has an additional settings file that we
     need to look for and load as well.
     """
-    return additional_configs.get(environ.get(env))
+    return _additional_config.get(environ.get(env))
 
-def load_config_from_file(app,filepath):
+
+def load_config_from_file(app, filepath):
     try:
         app.config.from_pyfile(filepath)
     except IOError:
-        # FIXME: Can we print to sys.stderr in production? Should this go to
+        # TODO: Can we print to sys.stderr in production? Should this go to
         # logs instead?
         print >> sys.stderr, "Did not find settings file %s" % filepath
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,28 +3,29 @@
 import unittest
 from os import environ
 from flask import Flask
-from coaster.app import additional_configs, configure, load_config_from_file, additional_settings_file
+from coaster.app import _additional_config, configure, load_config_from_file, additional_settings_file
+
 
 class TestCoasterUtils(unittest.TestCase):
     def setUp(self):
         self.app = Flask(__name__, instance_relative_config=True)
 
     def test_load_config_from_file(self):
-        load_config_from_file(self.app,"settings.py")
+        load_config_from_file(self.app, "settings.py")
         self.assertEqual(self.app.config['SETTINGS_KEY'], "settings")
 
     def test_additional_settings_from_file(self):
         env = 'COASTER_ENV'
-        environ[env]="gibberish"
-        self.assertEqual(additional_settings_file(env),None)
-        for k,v in additional_configs.items():
+        environ[env] = "gibberish"
+        self.assertEqual(additional_settings_file(env), None)
+        for k, v in _additional_config.items():
             environ[env] = k
-            self.assertEqual(additional_settings_file(env),v)
+            self.assertEqual(additional_settings_file(env), v)
 
     def test_configure(self):
         env = 'COASTER_ENV'
         environ[env] = "testing"
-        configure(self.app,env)
+        configure(self.app, env)
         self.assertEqual(self.app.config['SETTINGS_KEY'], "settings")
         self.assertEqual(self.app.config['TEST_KEY'], "test")
 
