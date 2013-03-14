8309d6841c5b5076ca63306226f43b8ff93af905
Kiran Jonnalagadda <kiran@hasgeek.com>
2013-03-15 01:15:33 +0530

    Move version into a separate file.

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 import os
+import re
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = unicode(open(os.path.join(here, 'README.rst')).read(), 'utf-8')
 CHANGES = unicode(open(os.path.join(here, 'CHANGES.rst')).read(), 'utf-8')
+versionfile = open(os.path.join(here, "coaster", "_version.py")).read()
+
+mo = re.search(r"^__version__\s*=\s*['\"]([^'\"]*)['\"]", versionfile, re.M)
+if mo:
+    version = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in coaster/_version.py.")
+
 
 requires = [
     'Flask',
@@ -14,10 +23,10 @@ requires = [
     ]
 
 setup(name='coaster',
-      version='0.3.9',
-      description='Coaster for Flask',
-      long_description=README + '\n\n' + CHANGES,
-      classifiers=[
+    version=version,
+    description='Coaster for Flask',
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
@@ -27,13 +36,13 @@ setup(name='coaster',
         "Development Status :: 3 - Alpha",
         "Topic :: Software Development :: Libraries",
         ],
-      author='Kiran Jonnalagadda',
-      author_email='kiran@hasgeek.com',
-      url='https://github.com/hasgeek/coaster',
-      keywords='coaster',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=True,
-      test_suite='tests',
-      install_requires=requires,
-      )
+    author='Kiran Jonnalagadda',
+    author_email='kiran@hasgeek.com',
+    url='https://github.com/hasgeek/coaster',
+    keywords='coaster',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=True,
+    test_suite='tests',
+    install_requires=requires,
+    )
