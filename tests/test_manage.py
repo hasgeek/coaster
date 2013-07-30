d85c035a5c26477002d5513b89322e1be66f7cd6
kracekumar <me@kracekumar.com>
2013-07-30 12:00:14 +0530

    Removed create

--- tests/test_manage.py
+++ tests/test_manage.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import unittest
-from coaster.manage import init_manager, create, set_alembic_revision
+from coaster.manage import init_manager, set_alembic_revision
 import coaster
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask import Flask
@@ -10,14 +10,17 @@ from flask import Flask
 class TestManagePy(unittest.TestCase):
     def setUp(self):
         self.app = Flask(__name__)
+        self.app.config.from_pyfile('development.py')
         self.db = SQLAlchemy(self.app)
+
+        class Test(self.db.Model):
+            __table__ = "test"
+            name = self.db.Column(self.db.Unicode(32))
+        self.Test = Test
         self.manage = init_manager(self.app, self.db, self.init_for)
 
     def init_for(self, env):
         coaster.app.init_app(self.app, env)
-
-    def test_db_create(self):
-        create('dev')
 
     def test_set_alembic_revision(self):
         set_alembic_revision(path='tests/alembic')
