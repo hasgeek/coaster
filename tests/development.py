5200a16f62d65cfa3d1acdd8f7757d54708d6050
kracekumar <me@kracekumar.com>
2013-08-12 15:35:38 +0530

    Added missing development.py

---
+++ tests/development.py
@@ -0,0 +1,12 @@
+"""
+Configuration used by coaster test suite
+"""
+SETTINGS_KEY = 'settings'
+ADMINS = ['test@example.com', ]
+DEFAULT_MAIL_SENDER = ('HasGeek', 'test@example.com')
+MAIL_SERVER = 'mail.example.com'
+MAIL_PORT = 587
+MAIL_USERNAME = 'username'
+MAIL_PASSWORD = 'PASSWORD'
+SECRET_KEY = 'd vldvnvnvjn'
+SQLALCHEMY_DATABASE_URI = 'postgres://:@localhost:5432/coaster'
