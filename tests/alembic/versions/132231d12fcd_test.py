f3931dd3eecdfa6be81273007663d4983aad5180
Kiran Jonnalagadda <kiran@hasgeek.com>
2014-03-08 23:41:00 +0530

    Fix for SQLAlchemy unicode warning

--- tests/alembic/versions/132231d12fcd_test.py
+++ tests/alembic/versions/132231d12fcd_test.py
@@ -7,7 +7,7 @@ Create Date: 2013-04-27 11:09:23.896698
 """
 
 # revision identifiers, used by Alembic.
-revision = '132231d12fcd'
+revision = u'132231d12fcd'
 down_revision = None
 
 from alembic import op
