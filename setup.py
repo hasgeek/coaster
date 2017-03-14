f0472d9567423ac8ae3a5a7bdac132ce5c809f48
Shreyas Satish <shreyas.satish@gmail.com>
2017-03-14 19:06:57 +0530

    bump html5lib to 0.999999999

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ requires = [
     'tldextract',
     'Pygments',
     'bleach',
-    'html5lib>=0.99999999',
+    'html5lib>=0.999999999',
     'markdown>=2.4.1',
     'pytz',
     'semantic_version',
