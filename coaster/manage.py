43fd96b42c2a5b19f8a48ef7a8fab08ae1468139
Kiran Jonnalagadda <kiran@hasgeek.com>
2017-05-04 18:15:26 +0530

    Replace Flask-Alembic with Flask-Migrate. Fixes #59.
    All apps must include corresponding changes to use Flask-Migrate.

--- coaster/manage.py
+++ coaster/manage.py
@@ -8,11 +8,10 @@ from alembic.script import ScriptDirectory
 from alembic.util import CommandError
 from flask_script import Manager, prompt_bool, Shell
 from flask_script.commands import Clean, ShowUrls
-from flask_alembic import ManageMigrations
+from flask_migrate import MigrateCommand
 
 
 manager = Manager()
-database = Manager(usage="Perform database operations")
 
 
 def alembic_table_metadata():
@@ -27,7 +26,7 @@ def set_alembic_revision(path=None):
     """Create/Update alembic table to latest revision number"""
     config = Config()
     try:
-        config.set_main_option("script_location", path or "alembic")
+        config.set_main_option("script_location", path or "migrations")
         script = ScriptDirectory.from_config(config)
         head = script.get_current_head()
         # create alembic table
@@ -47,16 +46,19 @@ def set_alembic_revision(path=None):
         stdout.write(e.message)
 
 
-@database.command
-def drop(env):
+@manager.command
+def dropdb():
     "Drop database tables"
     manager.db.engine.echo = True
     if prompt_bool("Are you sure you want to lose all your data"):
         manager.db.drop_all()
+        metadata, alembic_version = alembic_table_metadata()
+        alembic_version.drop()
+        manager.db.session.commit()
 
 
-@database.command
-def create(env):
+@manager.command
+def createdb():
     "Create database tables from sqlalchemy models"
     manager.db.engine.echo = True
     manager.db.create_all()
@@ -99,7 +101,10 @@ def init_manager(app, db, **kwargs):
     manager.app = app
     manager.db = db
     manager.context = kwargs
-    manager.add_command('db', database)
+    manager.add_command('db', MigrateCommand)
     manager.add_command('clean', Clean())
     manager.add_command('showurls', ShowUrls())
+    manager.add_command('shell', Shell(make_context=shell_context))
+    manager.add_command('plainshell', Shell(make_context=shell_context,
+        use_ipython=False, use_bpython=False))
     return manager
