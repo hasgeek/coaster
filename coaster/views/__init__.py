6295662f2e8408b57e99c2af8c2b27b3e4682dcf
Kiran Jonnalagadda <kiran@hasgeek.com>
2018-02-07 00:26:56 +0530

    Break up views into submodules

---
+++ coaster/views/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+View helpers
+============
+
+Coaster provides classes, functions and decorators for common scenarios in view handlers.
+"""
+
+from __future__ import absolute_import
+from .misc import *  # NOQA
+from .decorators import *  # NOQA
