2904d58f15885f01b4db98b996bef679d52256ca
Kiran Jonnalagadda <kiran@hasgeek.com>
2019-05-30 22:17:28 +0530

    Remove unnecessary import

--- coaster/db.py
+++ coaster/db.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import absolute_import, print_function
+from __future__ import absolute_import
 from flask_sqlalchemy import SQLAlchemy
 
 from sqlalchemy import event
