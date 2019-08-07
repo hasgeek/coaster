d89620f5d0ce42429e8cf68a3abc11a73946e05e
Kiran Jonnalagadda <kiran@hasgeek.com>
2019-08-07 11:41:53 +0530

    Data verification properties for CoordinatesMixin

--- tests/test_sqlalchemy_coordinates.py
+++ tests/test_sqlalchemy_coordinates.py
@@ -3,14 +3,11 @@
 from __future__ import absolute_import
 
 import unittest
-import warnings
-
-import sqlalchemy.exc
 
 from coaster.db import db
 from coaster.sqlalchemy import BaseMixin, CoordinatesMixin
 
-from .test_models import app1, app2
+from .test_models import app2
 
 
 class CoordinatesData(BaseMixin, CoordinatesMixin, db.Model):
@@ -21,7 +18,8 @@ class CoordinatesData(BaseMixin, CoordinatesMixin, db.Model):
 
 
 class TestCoordinatesColumn(unittest.TestCase):
-    app = app1
+    # Restrict tests to PostgreSQL as SQLite3 doesn't have a Decimal type
+    app = app2
 
     def setUp(self):
         self.ctx = self.app.test_request_context()
@@ -41,22 +39,30 @@ class TestCoordinatesColumn(unittest.TestCase):
 
     def test_columns_when_null(self):
         data = CoordinatesData()
-        self.assertEqual(data.coordinates, (None, None))
+        assert data.coordinates == (None, None)
+        assert data.has_coordinates is False
+
+    def test_columns_when_missing(self):
+        data = CoordinatesData()
+        assert data.has_coordinates is False
+        assert data.has_missing_coordinates is True
+        data.coordinates = (12, None)
+        assert data.has_coordinates is False
+        assert data.has_missing_coordinates is True
+        data.coordinates = (None, 73)
+        assert data.has_coordinates is False
+        assert data.has_missing_coordinates is True
+        data.coordinates = (12, 73)
+        assert data.has_coordinates is True
+        assert data.has_missing_coordinates is False
 
     def test_column_set_value(self):
-        warnings.simplefilter('ignore', category=sqlalchemy.exc.SAWarning)
-
         data = CoordinatesData()
         data.coordinates = (12, 73)
-        self.assertEqual(data.coordinates, (12, 73))
+        assert data.coordinates == (12, 73)
+        assert data.has_coordinates is True
         db.session.add(data)
         db.session.commit()
 
         readdata = CoordinatesData.query.first()
-        self.assertEqual(readdata.coordinates, (12, 73))
-
-        warnings.resetwarnings()
-
-
-class TestCoordinatesColumn2(TestCoordinatesColumn):
-    app = app2
+        assert readdata.coordinates == (12, 73)
