bdf133ebc89bcad511422c29430599fea0f7ca09
Kiran Jonnalagadda <kiran@hasgeek.com>
2020-07-31 04:45:49 +0530

    Rename HasGeek to Hasgeek

--- tests/settings.py
+++ tests/settings.py
@@ -5,7 +5,7 @@ Note: This is a test config file used by test_app.py.
 """
 SETTINGS_KEY = 'settings'
 ADMINS = ['test@example.com']
-DEFAULT_MAIL_SENDER = ('HasGeek', 'test@example.com')
+DEFAULT_MAIL_SENDER = ('Hasgeek', 'test@example.com')
 MAIL_SERVER = 'mail.example.com'
 MAIL_PORT = 587
 MAIL_USERNAME = 'username'
