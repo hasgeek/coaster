hasgeek/coaster

commit 99c223c3e3cc5861de47f97f12b34d183f8d3eb3

Support conditional grant of roles (implements #415) (#451)

src/coaster/sqlalchemy/statemanager.py
@@ -731,9 +731,8 @@ class StateTransitionWrapper(Generic[_P, _R, _T]):
         # Change the state for each of the state managers
         for statemanager, conditions in self.statetransition.transitions.items():
             if conditions.to is not None:  # Allow to=None for the @requires decorator
-                statemanager._set_state_value(
-                    self.obj, conditions.to.value
-                )  # Change state
+                # Change state
+                statemanager._set_state_value(self.obj, conditions.to.value)
         # Send a transition-after signal
         transition_after.send(self.obj, transition=self.statetransition)
         return result
@@ -1158,7 +1157,7 @@ class StateManagerInstance(Generic[_SM, _T]):
         """
         # Mypy complains because it can't infer that self.obj is RoleMixin instance.
         if isinstance(self.obj, RoleMixin):
-            proxy = self.obj.access_for(roles=roles, actor=actor, anchors=anchors)
+            proxy = self.obj.access_for(actor=actor, roles=roles, anchors=anchors)
             return {
                 name: transition
                 for name, transition in self.transitions(current=False).items()

tests/coaster_tests/sqlalchemy_roles_test.py
@@ -6,8 +6,8 @@
 from __future__ import annotations
 
 import json
-import unittest
-from collections.abc import MutableSet, Sequence
+from collections.abc import Iterable, MutableSet, Sequence
+from dataclasses import dataclass
 from typing import Any, Optional
 
 import pytest
@@ -26,6 +26,7 @@ from sqlalchemy.orm import (
 from coaster.sqlalchemy import (
     BaseMixin,
     BaseNameMixin,
+    ConditionalRole,
     DynamicAssociationProxy,
     LazyRoleSet,
     RoleAccessProxy,
@@ -33,6 +34,7 @@ from coaster.sqlalchemy import (
     RoleMixin,
     UuidMixin,
     relationship,
+    role_check,
     with_roles,
 )
 from coaster.utils import InspectableSet
@@ -91,15 +93,15 @@ class RoleModel(DeclaredAttrMixin, RoleMixin, Model):
     # These annotations always add to anything specified in __roles__
 
     id: Mapped[int] = sa_orm.mapped_column(sa.Integer, primary_key=True)  # noqa: A003
-    name: Mapped[str] = with_roles(
-        sa_orm.mapped_column(sa.Unicode(250)), rw={'owner'}
-    )  # Specify read+write access
+    # Specify read+write access
+    name: Mapped[str] = with_roles(sa_orm.mapped_column(sa.Unicode(250)), rw={'owner'})
 
+    # Grant 'owner' and 'editor' write but not read access
     title: Mapped[str] = with_roles(
         mapped_column(sa.Unicode(250)),
         write={'owner', 'editor'},
         datasets={'minimal', 'extra', 'third'},  # 'third' is unique here
-    )  # Grant 'owner' and 'editor' write but not read access
+    )
 
     defval: Mapped[str] = with_roles(
         sa_orm.mapped_column(sa.Unicode(250), deferred=True), rw={'owner'}
@@ -1214,7 +1216,7 @@ class TestCoasterRoles(AppTestCase):
         assert list(child2.actors_with({'parent_role_shared'})) == []
 
 
-class TestLazyRoleSet(unittest.TestCase):
+class TestLazyRoleSet:
     """Tests for LazyRoleSet, isolated from RoleMixin"""
 
     class EmptyDocument(RoleMixin):
@@ -1453,3 +1455,136 @@ class TestLazyRoleSet(unittest.TestCase):
         role_membership = RoleMembership()
         assert issubclass(RoleMembership, RoleGrantABC)
         assert isinstance(role_membership, RoleGrantABC)
+
+
+class TestConditionalRole:
+    """Tests for conditional roles."""
+
+    @dataclass
+    class User:
+        name: str
+
+        def __repr__(self) -> str:
+            return f'{self.__class__.__qualname__}({self.name!r})'
+
+        def __hash__(self) -> int:
+            """Support hashing users for actors_with in tests."""
+            return hash(self.__repr__())
+
+    @dataclass
+    class RoleCheckModel(RoleMixin[User]):
+        public: bool
+        created_by: TestConditionalRole.User
+        owners: Optional[list[TestConditionalRole.User]] = None
+
+        __roles__ = {
+            'creator': {'granted_via': {'created_by': None}},
+            'owner': {'granted_by': ['owners']},
+        }
+
+        @role_check('reader', 'viewer')  # Takes multiple roles as necessary
+        def has_reader_role(self, actor: Optional[TestConditionalRole.User]) -> bool:
+            # If this object is public, everyone gets the 'reader' role
+            if self.public:
+                return True
+            # If not public, only creators and owners get the role
+            return actor == self.created_by or actor in (self.owners or [])
+
+        @has_reader_role.iterable
+        def _(self) -> Iterable[TestConditionalRole.User]:
+            # We can't reasonably enumerate all user accounts as the role is
+            # conditionally granted to everyone, so limit this to specific users
+            return [self.created_by] + (self.owners or [])
+
+        @role_check('owner')
+        def creator_is_owner(self, actor: Optional[TestConditionalRole.User]) -> bool:
+            """
+            Only validates the creator as owner, while failing everyone else.
+
+            No iterable for this check, to test behaviour in such a case.
+            """
+            return actor is self.created_by
+
+    user = User('user')
+    owner1 = User('owner1')
+    owner2 = User('owner2')
+    unrelated_user = User('unrelated_user')
+    public_obj = RoleCheckModel(public=True, created_by=user, owners=[owner1, owner2])
+    private_obj = RoleCheckModel(public=False, created_by=user, owners=[owner1])
+
+    # Expected results in a test for 'reader' role in `public_obj` and `private_obj`
+    expected_results_for_reader = [
+        (user, True, True),
+        (owner1, True, True),
+        (owner2, True, False),
+        (unrelated_user, True, False),
+        (None, True, False),  # Anonymous user
+    ]
+
+    def test_constructor(self) -> None:
+        assert isinstance(self.RoleCheckModel.has_reader_role, ConditionalRole)
+        assert isinstance(self.RoleCheckModel.creator_is_owner, ConditionalRole)
+
+    # --- Part 1, tests for RoleCheckModel.has_reader_role, a typical implementation
+
+    @pytest.mark.parametrize(
+        ('user', 'public_reader', 'private_reader'), expected_results_for_reader
+    )
+    def test_callable(
+        self, user: TestConditionalRole.User, public_reader: bool, private_reader: bool
+    ) -> None:
+        assert self.public_obj.has_reader_role(user) is public_reader
+        assert self.private_obj.has_reader_role(user) is private_reader
+
+    @pytest.mark.parametrize(
+        ('user', 'public_reader', 'private_reader'), expected_results_for_reader
+    )
+    def test_container(
+        self, user: TestConditionalRole.User, public_reader: bool, private_reader: bool
+    ) -> None:
+        assert (user in self.public_obj.has_reader_role) is public_reader
+        assert (user in self.private_obj.has_reader_role) is private_reader
+
+    @pytest.mark.parametrize(
+        ('user', 'public_reader', 'private_reader'), expected_results_for_reader
+    )
+    def test_roles_for(
+        self, user: TestConditionalRole.User, public_reader: bool, private_reader: bool
+    ) -> None:
+        assert ('reader' in self.public_obj.roles_for(user)) is public_reader
+        assert ('reader' in self.private_obj.roles_for(user)) is private_reader
+
+    @pytest.mark.parametrize(
+        ('user', 'public_reader', 'private_reader'), expected_results_for_reader
+    )
+    def test_roles_for_second_role(
+        self, user: TestConditionalRole.User, public_reader: bool, private_reader: bool
+    ) -> None:
+        assert ('viewer' in self.public_obj.roles_for(user)) is public_reader
+        assert ('viewer' in self.private_obj.roles_for(user)) is private_reader
+
+    def test_iterable(self) -> None:
+        assert list(self.public_obj.has_reader_role) == [
+            self.user,
+            self.owner1,
+            self.owner2,
+        ]
+        assert list(self.private_obj.has_reader_role) == [self.user, self.owner1]
+
+    # --- Part 2, tests for RoleCheckModel.creator_is_owner, a partial implementation
+
+    def test_owner_via_conditional_role(self) -> None:
+        # The conditional role check only returns True for `user`, not `owner1`
+        assert self.public_obj.created_by == self.user
+        assert self.public_obj.creator_is_owner(self.user) is True
+        assert self.public_obj.creator_is_owner(self.owner1) is False
+        assert self.public_obj.creator_is_owner(self.owner2) is False
+        assert self.public_obj.creator_is_owner(self.unrelated_user) is False
+        # However, using roles_for, we get a more comprehensive check across providers
+        assert 'owner' in self.public_obj.roles_for(self.user)
+        assert 'owner' in self.public_obj.roles_for(self.owner1)
+        assert 'owner' in self.public_obj.roles_for(self.owner2)
+        assert 'owner' not in self.public_obj.roles_for(self.unrelated_user)
+        # Since `creator_is_owner` did not provide an iterable, calling actors_with
+        # will not enumerate `user`
+        assert set(self.public_obj.actors_with({'owner'})) == {self.owner1, self.owner2}

pyproject.toml
@@ -138,6 +138,7 @@ disable = [
   'too-many-locals',
   'too-many-public-methods',
   'unused-argument',
+  'unsupported-membership-test',
   # These need some serious refactoring, so disabled for now
   'too-many-branches',
   'too-many-nested-blocks',

src/coaster/utils/classes.py
@@ -587,7 +587,7 @@ class InspectableSet(Generic[_C]):
         object.__setattr__(self, '_members', members if members is not None else set())
 
     def __repr__(self) -> str:
-        return f'self.__class__.__qualname__({self._members!r})'
+        return f'{self.__class__.__qualname__}({self._members!r})'
 
     def __hash__(self) -> int:
         return hash(self._members)

src/coaster/utils/misc.py
@@ -32,6 +32,7 @@ __all__ = [
     'get_email_domain',
     'getbool',
     'is_collection',
+    'is_dunder',
     'make_name',
     'md5sum',
     'namespace_from_url',
@@ -366,9 +367,8 @@ def make_name(
     'feedback'
     """
     name = text.replace('@', delim)
-    name = unidecode(name).replace(
-        '@', 'a'
-    )  # We don't know why unidecode uses '@' for 'a'-like chars
+    # We don't know why unidecode uses '@' for 'a'-like chars
+    name = unidecode(name).replace('@', 'a')
     name = str(
         delim.join(
             [
@@ -695,3 +695,13 @@ def nary_op(
     if doc is not None:
         inner.__doc__ = doc
     return inner
+
+
+def is_dunder(name: str) -> bool:
+    """Check if a __dunder__ name (copied from the enum module)."""
+    return (
+        len(name) > 4
+        and name[:2] == name[-2:] == '__'
+        and name[2] != '_'
+        and name[-3] != '_'
+    )

tests/coaster_tests/statemanager_test.py
@@ -741,9 +741,8 @@ class TestStateManager(AppTestCase):
         """State managers indicate the currently available transitions (using current_auth)"""
         assert self.post.state.DRAFT
         assert 'submit' not in self.post.state.transitions()
-        add_auth_attribute(
-            'user', 'author'
-        )  # Add a user using the string 'author' (see MyPost.roles_for)
+        # Add a user using the string 'author' (see MyPost.roles_for)
+        add_auth_attribute('user', 'author')
         assert 'submit' in self.post.state.transitions()
         self.post.state.transitions()['submit']()
         assert not self.post.state.DRAFT

tests/coaster_tests/views_classview_test.py
@@ -249,13 +249,11 @@ class ModelDocumentView(UrlForView, InstanceLoader, ModelView[ViewDocument]):
     ) -> Optional[ResponseReturnValue]:
         if access_token == 'owner-admin-secret':  # nosec
             add_auth_attribute('permissions', InspectableSet({'siteadmin'}))
-            add_auth_attribute(
-                'user', 'this-is-the-owner'
-            )  # See ViewDocument.permissions
+            # See ViewDocument.permissions
+            add_auth_attribute('user', 'this-is-the-owner')
         if access_token == 'owner-secret':  # nosec
-            add_auth_attribute(
-                'user', 'this-is-the-owner'
-            )  # See ViewDocument.permissions
+            # See ViewDocument.permissions
+            add_auth_attribute('user', 'this-is-the-owner')
         return super().before_request()
 
     @route('')
@@ -338,17 +336,14 @@ class GatedDocumentView(UrlForView, InstanceLoader, ModelView[ViewDocument]):
         self, access_token: Optional[str] = None
     ) -> Optional[ResponseReturnValue]:
         if access_token == 'owner-secret':  # nosec
-            add_auth_attribute(
-                'user', 'this-is-the-owner'
-            )  # See ViewDocument.permissions
+            # See ViewDocument.permissions
+            add_auth_attribute('user', 'this-is-the-owner')
         if access_token == 'editor-secret':  # nosec
-            add_auth_attribute(
-                'user', 'this-is-the-editor'
-            )  # See ViewDocument.permissions
+            # See ViewDocument.permissions
+            add_auth_attribute('user', 'this-is-the-editor')
         if access_token == 'another-owner-secret':  # nosec
-            add_auth_attribute(
-                'user', 'this-is-another-owner'
-            )  # See ViewDocument.permissions
+            # See ViewDocument.permissions
+            add_auth_attribute('user', 'this-is-another-owner')
         return super().before_request()
 
     @route('perm')

commit 379f130125464714205467161b38bc32de90471b

Pass anchors alongside actor everywhere (#455)
This is a breaking change for role_check, introduced in #451.

src/coaster/sqlalchemy/mixins.py
@@ -492,7 +492,7 @@ class UrlDict(abc.Mapping, Generic[_UR]):
         # 2. If the action requires specific roles, confirm overlap with current_roles
         # 3. Confirm the action does not require additional parameters
         # 4. Yield whatever passes the tests
-        current_roles = self.obj.roles_for(current_auth.actor)
+        current_roles = self.obj.roles_for(current_auth.actor, current_auth.anchors)
         for app, app_actions in self.obj.url_for_endpoints.items():
             # pylint: disable=protected-access
             if app is None or (

src/coaster/sqlalchemy/roles.py
@@ -298,6 +298,7 @@ def _actor_in_relationship(actor: ActorType, relationship: Any) -> bool:
 
 def _roles_via_relationship(
     actor: ActorType,
+    anchors: Sequence[Any],
     relationship: Any,
     actor_attr: Optional[ActorAttrType],
     wanted_roles: set[str],
@@ -314,7 +315,7 @@ def _roles_via_relationship(
         if isinstance(relationship, RoleMixin):
             offered_roles: Union[set[str], LazyRoleSet]
             # TODO: Cache this as we'll get a different LazyRoleSet each time
-            offered_roles = relationship.roles_for(actor)
+            offered_roles = relationship.roles_for(actor, anchors)
             if offer_map is not None:
                 offer_map_subset = {
                     original_role
@@ -415,6 +416,7 @@ class LazyRoleSet(abc.MutableSet):
     __slots__ = (
         'obj',
         'actor',
+        'anchors',
         '_present',
         '_not_present',
         '_scanned_granted_by',
@@ -422,10 +424,15 @@ class LazyRoleSet(abc.MutableSet):
     )
 
     def __init__(
-        self, obj: RoleMixin, actor: Optional[ActorType], initial: Iterable[str] = ()
+        self,
+        obj: RoleMixin,
+        actor: Optional[ActorType],
+        anchors: Sequence[Any] = (),
+        initial: Iterable[str] = (),
     ) -> None:
         self.obj = obj
         self.actor = actor
+        self.anchors = anchors
         #: Roles that the actor has (make a copy of initial set as it will be mutated)
         self._present: set[str] = set(initial)
         #: Roles the actor does not have
@@ -436,7 +443,10 @@ class LazyRoleSet(abc.MutableSet):
         self._contents_fully_evaluated = False
 
     def __repr__(self) -> str:  # pragma: no cover
-        return f'LazyRoleSet({self.obj!r}, {self.actor!r}, {self._present!r})'
+        return (
+            f'LazyRoleSet({self.obj!r}, {self.actor!r}, {self.anchors!r},'
+            f' {self._present!r})'
+        )
 
     # Base class MutableSet defines this as a classmethod. We need an instance method to
     # get self.obj and self.actor. Pylint doesn't like it and must be silenced
@@ -444,7 +454,7 @@ class LazyRoleSet(abc.MutableSet):
         self, it: Iterator[str]
     ) -> LazyRoleSet:
         """Make a copy, as required by the `MutableSet` base class."""
-        return LazyRoleSet(self.obj, self.actor, it)
+        return LazyRoleSet(self.obj, self.actor, self.anchors, it)
 
     def _role_is_present(self, role: str) -> bool:
         """Test whether a role has been granted to the bound actor."""
@@ -463,7 +473,7 @@ class LazyRoleSet(abc.MutableSet):
             if relattr not in self._scanned_granted_by:
                 relationship = self.obj._get_relationship(relattr)
                 if isinstance(relationship, ConditionalRoleBind):
-                    is_present = self.actor in relationship
+                    is_present = relationship(self.actor, self.anchors)
                 elif self.actor is not None:
                     is_present = _actor_in_relationship(self.actor, relationship)
                 else:
@@ -523,6 +533,7 @@ class LazyRoleSet(abc.MutableSet):
 
                 granted_roles = _roles_via_relationship(
                     self.actor,
+                    self.anchors,
                     relationship,
                     actor_attr,
                     possibly_granted_roles,
@@ -605,7 +616,7 @@ class LazyRoleSet(abc.MutableSet):
     def copy(self) -> LazyRoleSet:
         """Return a shallow copy of the :class:`LazyRoleSet`."""
         # pylint: disable=protected-access
-        result = LazyRoleSet(self.obj, self.actor, self._present)
+        result = LazyRoleSet(self.obj, self.actor, self.anchors, self._present)
         result._not_present = set(self._not_present)
         result._scanned_granted_by = self._scanned_granted_by
         result._contents_fully_evaluated = self._contents_fully_evaluated
@@ -1250,7 +1261,9 @@ _CRA = TypeVar('_CRA')  # Actor type for ConditionalRole
 
 def role_check(
     *roles: str,
-) -> Callable[[Callable[[_CRM, Optional[_CRA]], bool]], ConditionalRole[_CRM, _CRA]]:
+) -> Callable[
+    [Callable[[_CRM, Optional[_CRA], Sequence[Any]], bool]], ConditionalRole[_CRM, _CRA]
+]:
     """
     Register a method that takes an actor and confirms if they have the given roles.
 
@@ -1260,12 +1273,14 @@ def role_check(
         class MyModel(RoleMixin, ...):
             ...
             @role_check('reader', 'viewer')  # Takes multiple roles as necessary
-            def has_reader_role(self, actor: Optional[ActorType]) -> bool:
+            def has_reader_role(
+                self, actor: Optional[ActorType], anchors: Sequence[Any] = ()
+            ) -> bool:
                 # If this object is public, everyone gets the 'reader' role
                 if self.state.PUBLIC:
                     return True
                 # If not public, only creators and owners get the role
-                return self.roles_for(actor).has_any(('creator', 'owner'))
+                return self.roles_for(actor, anchors).has_any(('creator', 'owner'))
 
             # A complementing iterable of all actors with the role is optional
             @has_reader_role.iterable
@@ -1290,7 +1305,7 @@ def role_check(
     """
 
     def decorator(
-        func: Callable[[_CRM, Optional[_CRA]], bool]
+        func: Callable[[_CRM, Optional[_CRA], Sequence[Any]], bool]
     ) -> ConditionalRole[_CRM, _CRA]:
         return ConditionalRole(roles, func)
 
@@ -1307,7 +1322,9 @@ class ConditionalRole(Generic[_CRM, _CRA]):
     __slots___ = ('__weakref__', 'name', 'check_func', 'iter_func', '__wrapped__')
 
     def __init__(
-        self, roles: Iterable[str], func: Callable[[_CRM, Optional[_CRA]], bool]
+        self,
+        roles: Iterable[str],
+        func: Callable[[_CRM, Optional[_CRA], Sequence[Any]], bool],
     ) -> None:
         self.roles = roles
         self.check_func = self.__wrapped__ = func
@@ -1321,8 +1338,10 @@ class ConditionalRole(Generic[_CRM, _CRA]):
             owner.__roles__.setdefault(role, {}).setdefault('granted_by', [])
             owner.__roles__[role]['granted_by'].append(name)
 
-    def __call__(self, __obj: _CRM, actor: Optional[_CRA]) -> bool:
-        return self.check_func(__obj, actor)
+    def __call__(
+        self, __obj: _CRM, actor: Optional[_CRA], anchors: Sequence[Any] = ()
+    ) -> bool:
+        return self.check_func(__obj, actor, anchors)
 
     def iterable(self, func: Callable[[_CRM], Iterable[_CRA]]) -> Self:
         """Register a method that returns an iterable of actors with the role."""
@@ -1361,11 +1380,11 @@ class ConditionalRoleBind(abc.Container, abc.Iterable, Generic[_CRM, _CRA]):
     def __getattr__(self, name: str) -> Any:
         return getattr(self._rolecheck, name)
 
-    def __call__(self, actor: Optional[_CRA]) -> bool:
-        return self._rolecheck.check_func(self.__self__, actor)
+    def __call__(self, actor: Optional[_CRA], anchors: Sequence[Any] = ()) -> bool:
+        return self._rolecheck.check_func(self.__self__, actor, anchors)
 
     def __contains__(self, __actor: Any) -> bool:
-        return self._rolecheck.check_func(self.__self__, __actor)
+        return self._rolecheck.check_func(self.__self__, __actor, ())
 
     def __iter__(self) -> Iterator[_CRA]:
         iter_func = self._rolecheck.iter_func
@@ -1445,34 +1464,21 @@ class RoleMixin(Generic[ActorType]):
         self, actor: Optional[ActorType] = None, anchors: Sequence[Any] = ()
     ) -> LazyRoleSet:
         """
-        Return roles available to the given ``actor`` or ``anchors`` on this object.
+        Return roles available to the given ``actor`` and/or ``anchors`` on this object.
 
-        The data type for both parameters are intentionally undefined here. Subclasses
-        are free to define them in any way appropriate. Actors and anchors are assumed
-        to be valid.
+        The actor must be of the same datatype as specified in the generic arg to
+        :class:`RoleMixin`. The datatype of anchors is intentionally undefined here.
 
         The role ``all`` is always granted. If ``actor`` is specified, the role ``auth``
         is granted. If not, ``anon`` is granted.
 
-        Subclasses overriding :meth:`roles_for` must always call :func:`super` to ensure
-        they receive a :class:`LazyRoleSet` that includes the standard roles and
-        evaluates for declarative roles when they are first accessed. Recommended
-        boilerplate::
-
-            def roles_for(
-                self, actor: Optional[User] = None, anchors: Sequence[Any] = ()
-            ) -> LazyRoleSet:
-                roles = super().roles_for(actor, anchors) # `roles` is set-like. Add
-                more roles here... return roles
-
-        Custom roles added in :meth:`roles_for` may not be required by the caller.
-        Instead of overriding :meth:`roles_for`, use :func:`role_check` to allow for
-        lazy evaluation when the role is actually needed.
+        Subclasses must avoid overriding :meth:`roles_for`. If a role must be
+        conditionally granted, use the :func:`role_check` decorator.
         """
         if actor is None:
-            result = LazyRoleSet(self, actor, {'all', 'anon'})
+            result = LazyRoleSet(self, actor, anchors, {'all', 'anon'})
         else:
-            result = LazyRoleSet(self, actor, {'all', 'auth'})
+            result = LazyRoleSet(self, actor, anchors, {'all', 'auth'})
         return result
 
     @property
@@ -1669,8 +1675,8 @@ class RoleMixin(Generic[ActorType]):
         self,
         *,
         actor: Optional[ActorType] = None,
-        roles: Optional[Union[LazyRoleSet, set[str]]] = None,
         anchors: Sequence[Any] = (),
+        roles: Optional[Union[LazyRoleSet, set[str]]] = None,
         datasets: Optional[Sequence[str]] = None,
     ) -> RoleAccessProxy:
         """

src/coaster/sqlalchemy/statemanager.py
@@ -1157,7 +1157,7 @@ class StateManagerInstance(Generic[_SM, _T]):
         """
         # Mypy complains because it can't infer that self.obj is RoleMixin instance.
         if isinstance(self.obj, RoleMixin):
-            proxy = self.obj.access_for(actor=actor, roles=roles, anchors=anchors)
+            proxy = self.obj.access_for(actor=actor, anchors=anchors, roles=roles)
             return {
                 name: transition
                 for name, transition in self.transitions(current=False).items()

src/coaster/views/classview.py
@@ -1067,7 +1067,9 @@ def requires_roles(roles: set[str]) -> ReturnDecorator:
 
     def decorator(f: WrappedFunc) -> WrappedFunc:
         def is_available_here(context: ModelViewType) -> bool:
-            return context.obj.roles_for(current_auth.actor).has_any(roles)
+            return context.obj.roles_for(
+                current_auth.actor, current_auth.anchors
+            ).has_any(roles)
 
         def is_available(context: ModelViewType) -> bool:
             result = is_available_here(context)

tests/coaster_tests/sqlalchemy_roles_test.py
@@ -1256,7 +1256,7 @@ class TestLazyRoleSet:
         pass
 
     def test_initial(self) -> None:
-        r1 = LazyRoleSet(self.EmptyDocument(), self.User(), {'all', 'auth'})
+        r1 = LazyRoleSet(self.EmptyDocument(), self.User(), (), {'all', 'auth'})
         assert r1._present == {'all', 'auth'}
         assert r1._not_present == set()
 
@@ -1300,10 +1300,10 @@ class TestLazyRoleSet:
         """Confirm we support common set operations."""
         doc = self.Document()
         user = self.User()
-        r = LazyRoleSet(doc, user, {'all', 'auth'})
+        r = LazyRoleSet(doc, user, (), {'all', 'auth'})
         assert r == {'all', 'auth'}
-        assert r == LazyRoleSet(doc, user, {'all', 'auth'})
-        assert r != LazyRoleSet(doc, None, {'all', 'auth'})
+        assert r == LazyRoleSet(doc, user, (), {'all', 'auth'})
+        assert r != LazyRoleSet(doc, None, (), {'all', 'auth'})
         assert len(r) == 2
         assert 'all' in r
         assert 'random' not in r
@@ -1317,12 +1317,14 @@ class TestLazyRoleSet:
         assert r >= {'all'}
         assert r > {'all'}
         assert not r > {'all', 'auth'}
-        assert r.union({'owner'}) == LazyRoleSet(doc, user, {'all', 'auth', 'owner'})
-        assert r | {'owner'} == LazyRoleSet(doc, user, {'all', 'auth', 'owner'})
+        assert r.union({'owner'}) == LazyRoleSet(
+            doc, user, (), {'all', 'auth', 'owner'}
+        )
+        assert r | {'owner'} == LazyRoleSet(doc, user, (), {'all', 'auth', 'owner'})
         assert r.union({'owner'}) == {'all', 'auth', 'owner'}
         assert r | {'owner'} == {'all', 'auth', 'owner'}
-        assert r.intersection({'all'}) == LazyRoleSet(doc, user, {'all'})
-        assert r & {'all'} == LazyRoleSet(doc, user, {'all'})
+        assert r.intersection({'all'}) == LazyRoleSet(doc, user, (), {'all'})
+        assert r & {'all'} == LazyRoleSet(doc, user, (), {'all'})
         assert r.intersection({'all'}) == {'all'}
         assert r & {'all'} == {'all'}
 
@@ -1337,7 +1339,7 @@ class TestLazyRoleSet:
         doc = self.Document()
         user = self.User()
         doc.user = user
-        r = LazyRoleSet(doc, user, {'all', 'auth'})
+        r = LazyRoleSet(doc, user, (), {'all', 'auth'})
 
         # At the start, the access flag is false and the cache sets are not populated
         assert r._present == {'all', 'auth'}
@@ -1483,7 +1485,9 @@ class TestConditionalRole:
         }
 
         @role_check('reader', 'viewer')  # Takes multiple roles as necessary
-        def has_reader_role(self, actor: Optional[TestConditionalRole.User]) -> bool:
+        def has_reader_role(
+            self, actor: Optional[TestConditionalRole.User], anchors: Sequence[Any] = ()
+        ) -> bool:
             # If this object is public, everyone gets the 'reader' role
             if self.public:
                 return True
@@ -1497,7 +1501,9 @@ class TestConditionalRole:
             return [self.created_by] + (self.owners or [])
 
         @role_check('owner')
-        def creator_is_owner(self, actor: Optional[TestConditionalRole.User]) -> bool:
+        def creator_is_owner(
+            self, actor: Optional[TestConditionalRole.User], anchors: Sequence[Any] = ()
+        ) -> bool:
             """
             Only validates the creator as owner, while failing everyone else.
 